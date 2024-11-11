c7b7d09e9ad159e9b626db4ca8d0422be3919bd9
Simonas <bataviciussimonas@gmail.com>
2024-11-11 22:07:35 +0200

    Project renamed.

---
+++ battery_dashboard_qt/build/Desktop_Qt_5_15_2_MinGW_32_bit-Release/release/qmlcache_loader.cpp
@@ -0,0 +1,165 @@
+#include <QtQml/qqmlprivate.h>
+#include <QtCore/qdir.h>
+#include <QtCore/qurl.h>
+
+namespace QmlCacheGeneratedCode {
+namespace _0x5f__BiGaugeSOC_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiDelayButton_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiGaugeBattCurrent_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiGauge_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiLabelN_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _vkBiStyle_QtQuick_VirtualKeyboard_Styles_vkBi_style_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiComboBox_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiGaugeCellVoltage_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiGaugeBattVoltage_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiApplicationRpi_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiApplication_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiRadioButton_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiCommIndicator_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiDashboard_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+namespace _0x5f__BiButton_qml { 
+    extern const unsigned char qmlData[];
+    const QQmlPrivate::CachedQmlUnit unit = {
+        reinterpret_cast<const QV4::CompiledData::Unit*>(&qmlData), nullptr, nullptr
+    };
+}
+
+}
+namespace {
+struct Registry {
+    Registry();
+    ~Registry();
+    QHash<QString, const QQmlPrivate::CachedQmlUnit*> resourcePathToCachedUnit;
+    static const QQmlPrivate::CachedQmlUnit *lookupCachedUnit(const QUrl &url);
+};
+
+Q_GLOBAL_STATIC(Registry, unitRegistry)
+
+
+Registry::Registry() {
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiGaugeSOC.qml"), &QmlCacheGeneratedCode::_0x5f__BiGaugeSOC_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiDelayButton.qml"), &QmlCacheGeneratedCode::_0x5f__BiDelayButton_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiGaugeBattCurrent.qml"), &QmlCacheGeneratedCode::_0x5f__BiGaugeBattCurrent_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiGauge.qml"), &QmlCacheGeneratedCode::_0x5f__BiGauge_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiLabelN.qml"), &QmlCacheGeneratedCode::_0x5f__BiLabelN_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/vkBiStyle/QtQuick/VirtualKeyboard/Styles/vkBi/style.qml"), &QmlCacheGeneratedCode::_vkBiStyle_QtQuick_VirtualKeyboard_Styles_vkBi_style_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiComboBox.qml"), &QmlCacheGeneratedCode::_0x5f__BiComboBox_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiGaugeCellVoltage.qml"), &QmlCacheGeneratedCode::_0x5f__BiGaugeCellVoltage_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiGaugeBattVoltage.qml"), &QmlCacheGeneratedCode::_0x5f__BiGaugeBattVoltage_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiApplicationRpi.qml"), &QmlCacheGeneratedCode::_0x5f__BiApplicationRpi_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiApplication.qml"), &QmlCacheGeneratedCode::_0x5f__BiApplication_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiRadioButton.qml"), &QmlCacheGeneratedCode::_0x5f__BiRadioButton_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiCommIndicator.qml"), &QmlCacheGeneratedCode::_0x5f__BiCommIndicator_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiDashboard.qml"), &QmlCacheGeneratedCode::_0x5f__BiDashboard_qml::unit);
+        resourcePathToCachedUnit.insert(QStringLiteral("/BiButton.qml"), &QmlCacheGeneratedCode::_0x5f__BiButton_qml::unit);
+    QQmlPrivate::RegisterQmlUnitCacheHook registration;
+    registration.version = 0;
+    registration.lookupCachedQmlUnit = &lookupCachedUnit;
+    QQmlPrivate::qmlregister(QQmlPrivate::QmlUnitCacheHookRegistration, &registration);
+}
+
+Registry::~Registry() {
+    QQmlPrivate::qmlunregister(QQmlPrivate::QmlUnitCacheHookRegistration, quintptr(&lookupCachedUnit));
+}
+
+const QQmlPrivate::CachedQmlUnit *Registry::lookupCachedUnit(const QUrl &url) {
+    if (url.scheme() != QLatin1String("qrc"))
+        return nullptr;
+    QString resourcePath = QDir::cleanPath(url.path());
+    if (resourcePath.isEmpty())
+        return nullptr;
+    if (!resourcePath.startsWith(QLatin1Char('/')))
+        resourcePath.prepend(QLatin1Char('/'));
+    return unitRegistry()->resourcePathToCachedUnit.value(resourcePath, nullptr);
+}
+}
+int QT_MANGLE_NAMESPACE(qInitResources_qml)() {
+    ::unitRegistry();
+    Q_INIT_RESOURCE(qml_qmlcache);
+    return 1;
+}
+Q_CONSTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qInitResources_qml))
+int QT_MANGLE_NAMESPACE(qCleanupResources_qml)() {
+    Q_CLEANUP_RESOURCE(qml_qmlcache);
+    return 1;
+}
+int QT_MANGLE_NAMESPACE(qInitResources_vkStyle)() {
+    ::unitRegistry();
+    Q_INIT_RESOURCE(vkStyle_qmlcache);
+    return 1;
+}
+Q_CONSTRUCTOR_FUNCTION(QT_MANGLE_NAMESPACE(qInitResources_vkStyle))
+int QT_MANGLE_NAMESPACE(qCleanupResources_vkStyle)() {
+    Q_CLEANUP_RESOURCE(vkStyle_qmlcache);
+    return 1;
+}
